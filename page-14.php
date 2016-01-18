895ed7aab90389644b5aee8dc310a20c04b968a1
jameshaddrill <jameshaddrill@gmail.com>
2016-01-18 16:58:25 +0000

    Notes

--- page-14.php
+++ page-14.php
@@ -1,15 +1,10 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying the home page.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package Test_theme
+ * @package arrg_theme
  */
 
 get_header(); ?>
