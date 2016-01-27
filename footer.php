fceeec9b1e7ece80439de906d383f7a30807ccc3
jameshaddrill <jameshaddrill@gmail.com>
2016-01-27 17:21:37 +0000

    Footer styles

--- footer.php
+++ footer.php
@@ -13,13 +13,11 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'test-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'test-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'test-theme' ), 'test-theme', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="site-footer" role="contentinfo">
+		<div class="content-area">
+			Footer
+		</div>
+	</footer>
 
 	<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
